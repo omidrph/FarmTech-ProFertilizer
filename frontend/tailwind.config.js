df1157305b5b9ce592162b33e6c36cf42df4008d
omidrph <rahmaniomid198@gmail.com>
2026-06-15 10:59:07 +0330

    update 1.1.3

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Vazirmatn', 'Samim', 'Sahel', 'system-ui', 'sans-serif'],
+        'vazir': ['Vazirmatn', 'sans-serif'],
+        'samim': ['Samim', 'sans-serif'],
+        'sahel': ['Sahel', 'sans-serif'],
+        'yekan': ['Yekan', 'sans-serif'],
+        'neda': ['Mj Neda', 'sans-serif'],
+        'tehran': ['Mj Tehran', 'sans-serif'],
+      },
+      fontWeight: {
+        'normal': '400',
+        'medium': '500',
+        'semibold': '600',
+        'bold': '700',
+        'black': '900',
+      }
+    },
   },
   plugins: [],
 }
-
